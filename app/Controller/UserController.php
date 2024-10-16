40d7f6b591da5be6641643c59073d2ffb4e09287
Aexey Kostenkov <l99shka@icloud.com>
2024-10-16 23:02:04 +0300

    Добавлен функционал авторизации по Телефону или Почте (в одном поле)  с валидными полями для Логина и Пароля, так же добавлена Yandex SmartCaptcha

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -38,7 +38,7 @@ class UserController
         ];
     }
 
-    private function isValidSignUp(array $data):array
+    private function isValidSignUp(array $data): array
     {
         $errors = [];
 
@@ -86,14 +86,101 @@ class UserController
 
     public function login(): array
     {
+        session_start();
+        $errors = [];
+        $errorsLoginPsw = [];
+
+        if (isset($_SESSION['user_id'])) {
+            header('Location: /');
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $errors = $this->isValidLogin($_POST);
+
+
+            if (empty($errors)) {
+                $login = $_POST['email-phone'];
+                $password = $_POST['password'];
+
+                if (filter_var($login, FILTER_VALIDATE_EMAIL)) {
+                    $user = User::getUserEmail($login);
+                } else {
+                    $user = User::getUserPhone($login);
+                }
+
+                if (!empty($user) && password_verify($password, $user->getPassword())) {
+                    $_SESSION['user_id'] = $user->getId();
+
+                    header('Location: /profile');
+                } else {
+                    $errorsLoginPsw = ['errors' => '* Неверный логин или пароль'];
+                }
+            }
+        }
+
         return [
             'view' => 'login',
-            'data' => []
+            'data' => [
+                'errors' => $errors,
+                'errorsLoginPsw' => $errorsLoginPsw
+            ]
         ];
     }
 
-    public function logout()
+    private function isValidLogin(array $data): array
     {
+        $errors = [];
 
+        if (empty($data['email-phone'])) {
+            $errors['email-phone'] = '* Ввведите E-mail или телефон';
+        }
+
+        if (empty($data['password'])) {
+            $errors['password'] = '* Введите пароль';
+        }
+
+        $token = $data['smart-token'];
+
+        if (!$this->check_captcha($token)) {
+            $errors['captcha'] = "* Пройдите каптчу";
+        }
+
+        return $errors;
+    }
+
+    public function check_captcha(string $token): bool
+    {
+        $user_ip = $_SERVER['REMOTE_ADDR'];
+        define('SMARTCAPTCHA_SERVER_KEY', 'ysc2_MqmtcEovTOg5lhVvNO8ECy6S3Agq3JzYZYhJqXEha8fe3dc1');
+
+        $ch = curl_init("https://smartcaptcha.yandexcloud.net/validate");
+        $args = [
+            "secret" => SMARTCAPTCHA_SERVER_KEY,
+            "token" => $token,
+            "ip" => "$user_ip"
+        ];
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpcode !== 200) {
+            return true;
+        }
+
+        $resp = json_decode($server_output);
+        return $resp->status === "ok";
+    }
+
+    public function logout(): void
+    {
+        session_start();
+
+        unset($_SESSION['user_id']);
+        header('Location: /');
     }
 }
