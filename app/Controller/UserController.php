aa8b5481871cbf22085bd9c9969170d66df829bd
Aexey Kostenkov <l99shka@icloud.com>
2024-10-16 22:19:04 +0300

    Добавлен функционал регистрации с уникальными валидными полями для Почты, Имени и Телефона, а так же валидные поля Паролей на совпадение

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -4,14 +4,84 @@ namespace App\Controller;
 
 
 
+use App\Model\User;
+
 class UserController
 {
     public function signup(): array
     {
+        session_start();
+        $errors = [];
+
+        if (isset($_SESSION['user_id'])) {
+            header('Location: /');
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $errors = $this->isValidSignUp($_POST);
+
+            if (empty($errors)) {
+                $password = $_POST['password'];
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $user = new User($_POST['name'], $_POST['email'], $_POST['phone'], $hash);
+
+                $user->save();
+
+                header('Location: /login');
+            }
+        }
+
         return [
             'view' => 'signup',
-            'data' => []
+            'data' => ['errors' => $errors]
         ];
+    }
+
+    private function isValidSignUp(array $data):array
+    {
+        $errors = [];
+
+        if (empty($data['email'])) {
+            $errors['email'] = '* Введите E-mail';
+        } else {
+            $userEmail = User::getUserEmail($data['email']);
+
+            if (!empty($userEmail)) {
+                $errors['email'] = '* Такой E-mail уже сущесвует';
+            }
+        }
+
+        if (empty($data['name'])) {
+            $errors['name'] = '* Введите Имя';
+        } else {
+            $userName = User::getUserName($data['name']);
+
+            if (!empty($userName)) {
+                $errors['name'] = '* Такое Имя уже сущесвует';
+            }
+        }
+
+        if (empty($data['phone'])) {
+            $errors['phone'] = '* Введите Телефон';
+        } else {
+            $userPhone = User::getUserPhone($data['phone']);
+
+            if (!empty($userPhone)) {
+                $errors['phone'] = '* Такой Телефон уже сущесвует';
+            }
+        }
+
+        if (empty($data['password'])) {
+            $errors['password'] = '* Введите пароль';
+        }
+
+        if (empty($data['psw'])) {
+            $errors['psw'] = '* Повторите пароль';
+        } elseif ($data['psw'] !== $data['password']) {
+            $errors['psw'] = '* Пароли не совпадают';
+        }
+        return $errors;
     }
 
     public function login(): array
--- app/Model/User.php
+++ app/Model/User.php
@@ -16,4 +16,125 @@ class User
         $this->phone = $phone;
         $this->password = $password;
     }
+
+    public static function getUserEmail(string $email):User|null
+    {
+        $stmt = ConnectFactory::create()->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        $user = new User($data['name'], $data['email'], $data['phone'], $data['password']);
+        $user->setId($data['id']);
+
+        return $user;
+    }
+
+    public static function getUserName(string $name):User|null
+    {
+        $stmt = ConnectFactory::create()->prepare("SELECT * FROM users WHERE name = :name");
+        $stmt->execute(['name' => $name]);
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        $user = new User($data['name'], $data['email'], $data['phone'], $data['password']);
+        $user->setId($data['id']);
+
+        return $user;
+    }
+
+    public static function getUserPhone(string $phone):User|null
+    {
+        $stmt = ConnectFactory::create()->prepare("SELECT * FROM users WHERE phone = :phone");
+        $stmt->execute(['phone' => $phone]);
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        $user = new User($data['name'], $data['email'], $data['phone'], $data['password']);
+        $user->setId($data['id']);
+
+        return $user;
+    }
+
+    public static function getUser(int $id):User|null
+    {
+        $stmt = ConnectFactory::create()->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        $user = new User($data['name'], $data['email'], $data['phone'], $data['password']);
+        $user->setId($data['id']);
+
+        return $user;
+    }
+
+    public function save(): void
+    {
+        $stmt = ConnectFactory::create()->prepare("INSERT INTO users (name, email, phone, password) VALUES (:name, :email, :phone, :password)");
+        $stmt->execute(['name' => $this->name, 'email' => $this->email, 'phone' => $this->phone, 'password' => $this->password]);
+    }
+
+    private function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName():string
+    {
+        return $this->name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail():string
+    {
+        return $this->email;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getPhone():string
+    {
+        return $this->phone;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPassword():string
+    {
+        return $this->password;
+    }
 }
